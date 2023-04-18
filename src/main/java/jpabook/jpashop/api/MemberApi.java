ab14a17d8fcd7c1b907e0792906bc55cb5dd1f23
Seonho Kim <sunhowold@naver.com>
2023-04-19 00:56:40 +0900

    API 개발 기본 - 회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApi.java
+++ src/main/java/jpabook/jpashop/api/MemberApi.java
@@ -2,13 +2,17 @@ package jpabook.jpashop.api;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.member.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -44,18 +48,46 @@ public class MemberApi {
 		return new CreateMemberResponse(id);
 	}
 
+	// PUT은 리소스를 완전히 교체하므로 멱등성을 보장한다.
+	// PATCH는 멱등하게, 혹은 멱등하지 않게 구현이 가능하다. 즉 멱등성을 보장하지 않는다.
+	@PutMapping("/api/v2/member/{id}")
+	public UpdateMemberResponse updateMemberV2(
+		@PathVariable("id") Long id,
+		@RequestBody @Valid UpdateMemberRequest request
+	) {
+		// 커맨드와 쿼리를 철저하게 분리한다. CQS(Command Query Separation)
+		// memberService.update 하면서 member를 리턴하면 update 하면서 query하는 꼴이 된다.
+		memberService.update(id, request.getName()); // Command
+
+		Member findMember = memberService.findOne(id); // Query
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
 	@Data
 	static class CreateMemberRequest {
 		private String name;
 	}
 
 	@Data
-	static class CreateMemberResponse{
-
+	static class CreateMemberResponse {
 		private Long id;
 
 		public CreateMemberResponse(Long id) {
 			this.id = id;
 		}
+	}
+
+	@Data
+	@NoArgsConstructor // Reflection을 위한 기본 생성자 추가
+	@AllArgsConstructor
+	private static class UpdateMemberRequest {
+		private String name;
+	}
+
+	@Data
+	@AllArgsConstructor
+	private static class UpdateMemberResponse {
+		private Long id;
+		private String name;
 	}
 }
