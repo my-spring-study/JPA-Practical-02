d5b345a6f4a3c017df7e25de7868b34fdb5cc37e
Seonho Kim <sunhowold@naver.com>
2023-04-23 23:24:15 +0900

    지연 로딩과 성능 최적화 - 간단한 주문 조회 V2: 엔티티를 DTO로 변환 & N + 1 문제

--- src/main/java/jpabook/jpashop/api/OrderSimpleApi.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApi.java
@@ -1,13 +1,17 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.order.Order;
+import jpabook.jpashop.domain.order.OrderStatus;
+import jpabook.jpashop.domain.type.Address;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -32,5 +36,39 @@ public class OrderSimpleApi {
 			order.getDelivery().getAddress(); // Lazy 강제 초기화
 		}
 		return all;
+	}
+
+	@GetMapping("/api/v2/simple-orders")
+	public List<SimpleOrderDto> ordersV2() {
+		// ORDER -> findAllByString에서 JPQL, 즉 SQL 1번 -> 결과 주문수 2 👉 쿼리 1번
+		// N+1(1+N)문제 -> 1 + 회원 N + 배송 N. 여기서 쿼리 '1'에서 가져오는 결과가 2개 이므로 N은 2
+		List<Order> findOrders = orderRepository.findAllByString(new OrderSearch());
+
+		// 주문 1의 Member, Delivery 조회 👉 쿼리 2번
+		// 주문 2의 Member, Delivery 조회 👉 쿼리 2번
+		// 따라서 총 쿼리 5번
+		List<SimpleOrderDto> dtos = findOrders
+			.stream()
+			.map(SimpleOrderDto::new)
+			.toList();
+
+		return dtos;
+	}
+
+	@Data
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) { // 별로 중요하지 않은 DTO 에서 중요한 엔티티 참조하는 것 괜찮다.
+			orderId = order.getId();
+			name = order.getMember().getName(); // LAZY 초기화
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress(); // LAZY 초기화
+		}
 	}
 }
