400b26a33032c73d4c9618e1cfb3bf826f991283
Seonho Kim <sunhowold@naver.com>
2023-05-09 21:51:08 +0900

    [컬렉션 조회 최적화] 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,9 +2,6 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -13,13 +10,13 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
-	@Bean
-	Hibernate5Module hibernate5Module() {
-		Hibernate5Module hibernate5Module = new Hibernate5Module();
-
-		//강제 지연 로딩 설정
-		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
-
-		return hibernate5Module;
-	}
+	// @Bean
+	// Hibernate5Module hibernate5Module() {
+	// 	Hibernate5Module hibernate5Module = new Hibernate5Module();
+	//
+	// 	//강제 지연 로딩 설정
+	// 	hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+	//
+	// 	return hibernate5Module;
+	// }
 }
--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.order.Order;
 import jpabook.jpashop.domain.order.OrderItem;
+import jpabook.jpashop.domain.order.OrderStatus;
+import jpabook.jpashop.domain.type.Address;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -30,5 +35,49 @@ public class OrderApiController {
 		}
 
 		return all;
+	}
+
+	@GetMapping("/api/v2/orders")
+	public List<OrderDto> ordersV2() {
+		// Order(1) -> Member(N) -> Delivery(N) -> OrderItem(1) -> Item(N)
+		return orderRepository.findAllByString(new OrderSearch()).stream()
+			.map(OrderDto::new)
+			.toList();
+	}
+
+	@Data
+	private static class OrderDto {
+
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+		private List<OrderItemDto> orderItems;
+
+		public OrderDto(Order order) {
+			orderId = order.getId();
+			name = order.getMember().getName();
+			orderDate = order.getOrderDate();
+			orderStatus = order.getStatus();
+			address = order.getDelivery().getAddress();
+			orderItems = order.getOrderItems().stream()
+				.map(OrderItemDto::new)
+				.toList();
+		}
+	}
+
+	@Getter
+	static class OrderItemDto {
+
+		private final String itemName;
+		private final int orderPrice;
+		private final int count;
+
+		public OrderItemDto(OrderItem orderItem) {
+			itemName = orderItem.getItem().getName();
+			orderPrice = orderItem.getOrderPrice();
+			count = orderItem.getCount();
+		}
 	}
 }
